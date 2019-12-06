6a33a64f0a33313646e8fbbd013946044ae8446e
pk <p.kaske@gmail.com>
2019-12-06 09:32:53 +0100

    Add firefox add-on, remove locales, fix readme.

--- README.md
+++ README.md
@@ -1,16 +1,5 @@
-# TV Enhance
-Chrome Extension to make working with TradingView charts a little bit more intuitive.
-
-## What can it do
-So far you're able to set custom keyboard shortcuts for drawing tools and some chart settings.
-
-## How to develop
-First install dependencies with `yarn install`.
-
-There is a gulp script that builds the contents of the options page as soon as something changes.
-Run it with `npm run dev`.
-
-Load the project as an unpacked extension in Chrome.
+# FTXFIX
+Chrome Extension to make working with FTX a little bit more intuitive.
 
 ## How to contribute
 PRs very welcome
---
+++ chrome/src/inject/inject.js
@@ -0,0 +1,22 @@
+chrome.extension.sendMessage({}, function(response) {
+	var readyStateCheckInterval = setInterval(function() {
+		if (document.readyState === "complete") {
+			clearInterval(readyStateCheckInterval);
+			addIds();
+			
+			document.addEventListener('click',() => {
+				addIds();
+			});
+		}
+	}, 10);
+});
+
+function addIds() {
+	if (window.location.href.startsWith('https://ftx.com/trade/')) {
+		document.querySelectorAll('header')[1].id = '___symbolSelector';
+		document.querySelector('[aria-label="open drawer"]').parentNode.id = '___drawerIconWrapper';
+	} else {
+		document.querySelectorAll('header')[1].id = '';
+		document.querySelector('[aria-label="open drawer"]').parentNode.id = '';
+	}
+}
